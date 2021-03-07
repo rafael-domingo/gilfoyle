8f312a9543a69ad83ffb1ae0f4cb0de81b5579cd
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-03-07 10:18:59 -0600

    Clean up code and fix warnings

---
+++ client/src/containers/Mobile/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Components
+import Title from '../../elements/Title';
+import Nav from '../../components/Nav';
+import Detail from '../../components/Detail';
+import Background from '../../elements/Background';
