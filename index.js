9bc91158cf647ef26863b7e5ff26d4714ef04bd3
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-03-07 20:15:19 -0600

    Configure React App

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const cryptoAPI = require('./crypto');
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 // Middleware
 app.use(cors());
 
 app.use('/', cryptoAPI);
 
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
+  
 app.listen(PORT, () => console.log('Server started'));
